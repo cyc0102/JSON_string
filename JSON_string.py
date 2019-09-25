ae5462a0e90971fdc876305d23f5feb32e281e34
Bryan <tw_bryan_chen@intretech.com>
2019-09-25 10:07:05 +0800

    Update JSON_string.py

--- JSON_string.py
+++ JSON_string.py
@@ -3,7 +3,8 @@ import time
 import calendar
 names = ["Unknown","Jackie Chan","Jet Li","zoe","Unknown"]
 locations = [(1,2),(3,4),(5,6),(7,8)]
-recognized_dic = {"Jacke Chan":(3,4),"Jet Li":(5,6),"Unknown": [(1,2),(7,8)]}
+recognized_dic = {"Unknown": [(1,2),(7,8)],"Jacke Chan":(3,4),"Jet Li":(5,6)}
+
 # a Python object (dict)
 x = {"timestamp":  calendar.timegm(time.gmtime()) ,"recognized": recognized_dic}
 # convert into JSON:
